87bbc340a940c2633fe44e20198907ee3b3d8ee5
Hanna Lee <mani21na@gmail.com>
2020-02-07 21:26:15 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'appointments/show'
-
-  get 'patients/show'
-
-  get 'doctors/show'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors
   resources :patients
