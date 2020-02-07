d15b6a8f776c67b42a3234d8c54f145bdd7fa9b9
Hanna Lee <mani21na@gmail.com>
2020-02-07 21:32:42 +0000

    Automatically backed up by Learn

--- spec/features/doctors_spec.rb
+++ spec/features/doctors_spec.rb
@@ -1,28 +1,21 @@
 require 'rails_helper'
 
-describe 'Doctor' do
-
+describe "doctors", type: :feature do
   before do
-    @mcdreamy = Doctor.create(name: "Derek Shepherd", department: "Neurosurgery")
-    @patient1 = Patient.create(name: "Bill Gates", age: 60)
-    @patient2 = Patient.create(name: "Russell Wilson", age: 27)
-    @appointment1 = Appointment.create(appointment_datetime: DateTime.new(2016, 10, 1), doctor: @mcdreamy, patient: @patient1  )
-    @appointment2 = Appointment.create(appointment_datetime: DateTime.new(2016, 10, 1), doctor: @mcdreamy, patient: @patient2  )
+    @meredith = Doctor.create({name: "Meredith Grey", department: "Internal Medicine"})
+    @bart = Patient.create(name: "Bart Simpson", age:10 )
+    Appointment.create(appointment_datetime: DateTime.new(2016, 01, 11, 20, 20, 0), patient_id: 1, doctor_id: 1)
   end
 
-  it 'has a name' do
-    expect(@mcdreamy.name).to eq("Derek Shepherd")
-  end
+  describe "#show page" do
+    it "shows all of a doctor's appointment times in a human readable format" do
+      visit doctor_path(@meredith)
+      expect(page).to have_content("January 11, 2016 at 20:20")
+    end
 
-  it 'has a department' do
-    expect(@mcdreamy.department).to eq("Neurosurgery")
-  end
-
-  it 'has many appointments' do
-    expect(@mcdreamy.appointments).to eq([@appointment1, @appointment2])
-  end
-
-  it 'has many patients, through appointments' do
-    expect(@mcdreamy.patients).to eq([@patient1, @patient2])
+    it "links to the patient's show page by name for each appointment" do
+      visit doctor_path(@meredith)
+      expect(page).to have_link("Bart Simpson", href: patient_path(@bart))
+    end
   end
 end
