f36e1fa131e432cfea1f9497af236c55e0256a9d
Hanna Lee <mani21na@gmail.com>
2020-02-07 21:25:31 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,8 +1,9 @@
-class Appointment < ApplicationRecord
+class Appointment < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :patient
 
   def appt_datetime
     appointment_datetime.strftime("%B %d, %Y at %H:%M")
   end
+
 end
