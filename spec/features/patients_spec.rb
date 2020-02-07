377af4bb1ce0b51ec96f5aa718087cccf84aff4c
Hanna Lee <mani21na@gmail.com>
2020-02-07 20:57:18 +0000

    Automatically backed up by Learn

--- spec/features/patients_spec.rb
+++ spec/features/patients_spec.rb
@@ -1,57 +1,23 @@
 require 'rails_helper'
 
-describe "patients", type:  :feature do
+describe "appointments", type:  :feature do
   before do
-    Doctor.create([
-      {name: "Meredith Grey", department: "Internal Medicine"},
-      {name: "Hawkeye Pierce", department: "Surgery"},
-      {name: "Leonard 'Bones' McCoy", department: "Internal Medicine"},
-      {name: "Phillip Chandler", department: "Internal Medicine"},
-      {name: "Michaela Quinn", department: "Internal Medicine"}
-    ])
-
-    Patient.create([
-      {name: "Homer Simpson", age:38},
-      {name: "Bart Simpson", age:10},
-      {name: "Marge Simpson", age:36},
-      {name: "Lisa Simpson", age:8},
-      {name: "Maggie Simpson", age:1},
-    ])
-
-    Appointment.create([
-      {appointment_datetime: DateTime.new(2016, 03, 15, 18, 00, 0), patient_id: 1, doctor_id: 2},
-      {appointment_datetime: DateTime.new(2016, 01, 11, 20, 20, 0), patient_id: 2, doctor_id: 1},
-      {appointment_datetime: DateTime.new(2016, 04, 22, 14, 00, 0), patient_id: 3, doctor_id:5 },
-      {appointment_datetime: DateTime.new(2017, 10, 30, 15, 00, 0), patient_id: 4, doctor_id:4 },
-      {appointment_datetime: DateTime.new(2016, 07, 11, 16, 00, 0), patient_id: 5, doctor_id: 3},
-      {appointment_datetime: DateTime.new(2016, 05, 31, 17, 00, 0), patient_id: 1, doctor_id: 2},
-      {appointment_datetime: DateTime.new(2017, 06, 03, 10, 00, 0), patient_id: 3, doctor_id: 1}
-      ])
-
-      @meredith = Doctor.first
-      @bart = Patient.find(2)
+    @hawkeye = Doctor.create({name: "Hawkeye Pierce", department: "Surgery"})
+    @homer = Patient.create({name: "Homer Simpson", age:38})
+    @appointment = Appointment.create({appointment_datetime: DateTime.new(2016, 03, 15, 18, 00, 0), patient: @homer, doctor: @hawkeye})
   end
 
-  describe "#index page" do
-    it 'lists all of the patients and the number of appointments they have' do
-      visit patients_path
-      within("ul") do
-        expect(page).to have_content("Name: Marge Simpson Number of Appointments: 2")
-        expect(page).to have_content("Name: Bart Simpson Number of Appointments: 1")
-      end
-    end
+  it "should display an appointment's doctor" do
+    visit appointment_path(@appointment)
+    expect(page).to have_link("Hawkeye Pierce", href: doctor_path(@hawkeye))
   end
 
-  describe "#show page" do
-    it "shows all of a patient's appointment times in a human readable format" do
-      visit patient_path(@bart)
-      expect(page).to have_content("January 11, 2016 at 20:20")
-    end
-
-    it "links to the doctor's show page by name for the doctor of each appointment" do
-      visit patient_path(@bart)
-      expect(page).to have_link("Meredith Grey", href: doctor_path(@meredith))
-    end
+  it "should display an appointment's patient" do
+    visit appointment_path(@appointment)
+    expect(page).to have_link("Homer Simpson", href: patient_path(@homer))
   end
 
+  it "should not have an index page" do
+    expect {visit('/appointments')}.to raise_error(ActionController::RoutingError)
+  end
 end
