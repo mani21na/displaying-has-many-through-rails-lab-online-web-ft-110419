098865d0b0e621347ab308050bac3aed69eef05e
Hanna Lee <mani21na@gmail.com>
2020-02-07 21:00:04 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200207205620) do
-
-  create_table "appointments", force: :cascade do |t|
-    t.datetime "appointment_datetime"
-    t.integer  "doctor_id"
-    t.integer  "patient_id"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-  end
+ActiveRecord::Schema.define(version: 20200207204850) do
 
   create_table "doctors", force: :cascade do |t|
     t.string   "name"
